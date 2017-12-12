b1af844edde2bd081e8e6b77880186204ad85ac3
German Mendoza <german.mendoza.187@gmail.com>
2017-12-12 16:32:31 -0600

    add controllers to create tutorial views

---
+++ DashWallet/PivxControllers/BaseController.swift
@@ -0,0 +1,61 @@
+//
+//  BaseController.swift
+//  BreadWallet
+//
+//  Created by German Mendoza on 9/22/17.
+//  Copyright © 2017 Aaron Voisine. All rights reserved.
+//
+
+import UIKit
+
+class BaseController: UIViewController {
+    
+    let cellIdentifier = "cellIdentifier"
+    let emptyIdentifier = "emptyIdentifier"
+    //var state:TableState = .success
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        setupNavigationBar()
+    }
+    
+    func setup(){}
+
+    func setupNavigationBar(){
+        navigationController?.navigationBar.backgroundColor = K.color.purple_r85g71b108
+        navigationItem.hidesBackButton = true
+        let backButton = UIBarButtonItem(image: UIImage(named:"icBack"), style: .plain, target: self, action: #selector(tappedBackButton))
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    func refresh(){}
+    
+    func addMenuButton(){
+        let menuButton = UIBarButtonItem(image: UIImage(named:"icDrawer"), style: .plain, target: self, action: #selector(tappedMenuButton))
+        navigationItem.hidesBackButton = true
+        navigationItem.leftBarButtonItem = menuButton
+        
+    }
+    
+    func addOptionButton(){
+        let optionButton = UIBarButtonItem(image: UIImage(named:"icMenuOptions"), style: .plain, target: self, action: #selector(tappedOptionButton))
+        navigationItem.rightBarButtonItem = optionButton
+    }
+    
+    func tappedOptionButton(){
+    
+    }
+    
+    func tappedMenuButton(){
+        //self.slideMenuController()?.openLeft()
+    }
+    
+    func tappedBackButton(){
+        let _ = navigationController?.popViewController(animated: true)
+    }
+}
