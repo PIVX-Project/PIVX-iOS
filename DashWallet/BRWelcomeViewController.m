c65e04dbae993e9d97a04c9826a2682dbcbdc90c
German Mendoza <german.mendoza.187@gmail.com>
2017-12-12 12:46:03 -0600

    add changes in welcomen controller, restore controller, and generate controller, add severall assets

--- DashWallet/BRWelcomeViewController.m
+++ DashWallet/BRWelcomeViewController.m
@@ -32,12 +32,9 @@
 @interface BRWelcomeViewController ()
 
 @property (nonatomic, assign) BOOL hasAppeared, animating;
-@property (nonatomic, strong) id foregroundObserver, backgroundObserver;
 
-@property (nonatomic, strong) IBOutlet UIView *wallpaper, *wallpaperContainer;
 @property (nonatomic, strong) IBOutlet UIButton *newwalletButton, *recoverButton;
-@property (nonatomic, strong) IBOutlet NSLayoutConstraint *logoXCenter, *walletXCenter, *restoreXCenter,
-                                                           *wallpaperXLeft;
+
 
 @end
 
@@ -48,37 +45,12 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-        
-    self.navigationController.delegate = self;
-
-    self.newwalletButton.titleLabel.adjustsFontSizeToFitWidth = YES;
-    self.recoverButton.titleLabel.adjustsFontSizeToFitWidth = YES;
-    
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
     self.newwalletButton.titleLabel.adjustsLetterSpacingToFitWidth = YES;
     self.recoverButton.titleLabel.adjustsLetterSpacingToFitWidth = YES;
 #pragma clang diagnostic pop
 
-    self.foregroundObserver =
-        [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationWillEnterForegroundNotification object:nil
-        queue:nil usingBlock:^(NSNotification *note) {
-            [self animateWallpaper];
-        }];
-    
-    self.backgroundObserver =
-        [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidEnterBackgroundNotification object:nil
-        queue:nil usingBlock:^(NSNotification *note) {
-            self.wallpaperXLeft.constant = 0;
-            [self.wallpaper.superview layoutIfNeeded];
-        }];
-}
-
-- (void)dealloc
-{
-    if (self.navigationController.delegate == self) self.navigationController.delegate = nil;
-    if (self.foregroundObserver) [[NSNotificationCenter defaultCenter] removeObserver:self.foregroundObserver];
-    if (self.backgroundObserver) [[NSNotificationCenter defaultCenter] removeObserver:self.backgroundObserver];
 }
 
 -(UIStatusBarStyle)preferredStatusBarStyle {
@@ -94,12 +66,6 @@
     [super viewWillAppear:animated];
 
     if (self.hasAppeared) {
-        self.logoXCenter.constant = self.view.frame.size.width;
-        self.navigationItem.titleView.hidden = NO;
-    }
-    else {
-        self.walletXCenter.constant = -self.view.frame.size.width;
-        self.restoreXCenter.constant = -self.view.frame.size.width;
     }
 }
 
@@ -123,48 +89,9 @@
         }
 
         if (! self.hasAppeared) {
-            [self.wallpaperContainer removeFromSuperview];
-            [self.navigationController.view insertSubview:self.wallpaperContainer atIndex:0];
-            [self.navigationController.view
-             addConstraint:[NSLayoutConstraint constraintWithItem:self.navigationController.view
-                                                        attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.wallpaperContainer
-                                                        attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0]];
-            [self.navigationController.view
-             addConstraint:[NSLayoutConstraint constraintWithItem:self.navigationController.view
-                                                        attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.wallpaperContainer
-                                                        attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0]];
             self.hasAppeared = YES;
-            self.logoXCenter.constant = self.view.frame.size.width;
-            self.walletXCenter.constant = 0.0;
-            self.restoreXCenter.constant = 0.0;
-            self.navigationItem.titleView.hidden = NO;
-            self.navigationItem.titleView.alpha = 0.0;
-
-            [UIView animateWithDuration:0.35 delay:1.0 usingSpringWithDamping:0.8 initialSpringVelocity:0.0
-            options:UIViewAnimationOptionCurveEaseOut animations:^{
-                self.navigationItem.titleView.alpha = 1.0;
-                [self.navigationController.view layoutIfNeeded];
-            } completion:nil];
         }
-
-        [self animateWallpaper];
     });
-}
-
-- (void)animateWallpaper
-{
-    if (self.animating) return;
-    self.animating = YES;
-
-    self.wallpaperXLeft.constant = -240.0;
-
-    [UIView animateWithDuration:30.0 delay:0.0
-    options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionRepeat | UIViewAnimationOptionAutoreverse
-    animations:^{
-        [self.wallpaperContainer layoutIfNeeded];
-    } completion:^(BOOL finished) {
-        self.animating = NO;
-    }];
 }
 
 // MARK: IBAction
@@ -185,61 +112,6 @@
     UIViewController *c = [self.storyboard instantiateViewControllerWithIdentifier:@"RecoverViewController"];
 
     [self.navigationController pushViewController:c animated:YES];
-}
-
-// MARK: UIViewControllerAnimatedTransitioning
-
-// This is used for percent driven interactive transitions, as well as for container controllers that have companion
-// animations that might need to synchronize with the main animation.
-- (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext
-{
-    return 0.35;
-}
-
-// This method can only be a nop if the transition is interactive and not a percentDriven interactive transition.
-- (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
-{
-    UIView *v = transitionContext.containerView;
-    UIViewController *to = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey],
-                     *from = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
-
-    to.view.center = CGPointMake(v.frame.size.width*(to == self ? -1 : 3)/2.0, to.view.center.y);
-    [v addSubview:to.view];
-    [v layoutIfNeeded];
-    
-    [UIView animateWithDuration:[self transitionDuration:transitionContext] delay:0.0 usingSpringWithDamping:0.8
-    initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
-        to.view.center = from.view.center;
-        from.view.center = CGPointMake(v.frame.size.width*(to == self ? 3 : -1)/2.0, from.view.center.y);
-//        [self.navigationController.view layoutIfNeeded];
-    } completion:^(BOOL finished) {
-        if (to == self) [from.view removeFromSuperview];
-        [transitionContext completeTransition:YES];
-    }];
-}
-
-// MARK: - UINavigationControllerDelegate
-
-- (id<UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController
-animationControllerForOperation:(UINavigationControllerOperation)operation fromViewController:(UIViewController *)fromVC
-toViewController:(UIViewController *)toVC
-{
-    return self;
-}
-
-//TODO: implement interactive transitions
-
-// MARK: - UIViewControllerTransitioningDelegate
-
-- (id<UIViewControllerAnimatedTransitioning>)animationControllerForPresentedController:(UIViewController *)presented
-presentingController:(UIViewController *)presenting sourceController:(UIViewController *)source
-{
-    return self;
-}
-
-- (id<UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:(UIViewController *)dismissed
-{
-    return self;
 }
 
 @end
---
+++ DashWallet/PivxUtils/PivxExtensions.swift
@@ -0,0 +1,59 @@
+//
+//  Extensions.swift
+//  BreadWallet
+//
+//  Created by German Mendoza on 9/22/17.
+//  Copyright © 2017 Aaron Voisine. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(_ red: CGFloat, green: CGFloat, blue: CGFloat, alpha:CGFloat = 1)->UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
+    }
+    
+    static func fromHexString(hex:String) -> UIColor {
+        var cString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if cString.characters.count != 6 {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor.rgb(CGFloat((rgbValue & 0xFF0000) >> 16), green: CGFloat((rgbValue & 0x00FF00) >> 8), blue: CGFloat(rgbValue & 0x0000FF))
+    }
+    
+}
+
+extension UIView {
+
+    func border(cornerRadius:CGFloat, color:UIColor? = nil){
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = cornerRadius
+        
+        if let unwrappedColor = color {
+            self.layer.borderWidth = 2
+            self.layer.borderColor = unwrappedColor.cgColor
+        }
+    }
+}
+
+extension Notification.Name {
+    
+    static let kReachabilityChangedNotification = Notification.Name("kNetworkReachabilityChangedNotification")
+    static let BRWalletBalanceChangedNotification = Notification.Name("BRWalletBalanceChangedNotification")
+    static let BRWalletManagerSeedChangedNotification = Notification.Name("BRWalletManagerSeedChangedNotification")
+    static let BRPeerManagerSyncStartedNotification = Notification.Name("BRPeerManagerSyncStartedNotification")
+    static let BRPeerManagerSyncFinishedNotification = Notification.Name("BRPeerManagerSyncFinishedNotification")
+    static let BRPeerManagerSyncFailedNotification = Notification.Name("BRPeerManagerSyncFailedNotification")
+    static let BRPeerManagerTxStatusNotification = Notification.Name("BRPeerManagerTxStatusNotification")
+}
