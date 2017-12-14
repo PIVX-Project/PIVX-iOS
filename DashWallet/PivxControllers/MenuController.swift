62a00f42629fae602dd7db0e263b1cc5faf73100
German Mendoza <german.mendoza.187@gmail.com>
2017-12-14 16:31:09 -0600

    add verification in menucontroler

--- DashWallet/PivxControllers/MenuController.swift
+++ DashWallet/PivxControllers/MenuController.swift
@@ -13,17 +13,22 @@ class MenuController: BaseController {
     @IBOutlet weak var syncImageView: UIImageView!
     @IBOutlet weak var syncLabel: UILabel!
     @IBOutlet weak var cotainerViewHeightConstraint: NSLayoutConstraint!
+    var optionSelected:Int = 1
     
     override func setup(){
         cotainerViewHeightConstraint.constant = K.main.height - 130
     }
 
     @IBAction func tappedMyWalletButton(_ sender: Any) {
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        let homeController = storyboard.instantiateViewController(withIdentifier: "RootViewController")
+        if optionSelected == 1 {
+            slideMenuController()?.closeLeft()
+            return
+        }
+        
         let homeController = RootController.shared
         let nav = UINavigationController(rootViewController: homeController)
         slideMenuController()?.changeMainViewController(nav, close: true)
+        optionSelected = 1
     }
     @IBAction func tappedAddressBookButton(_ sender: Any) {
 //        let controller = AddressContactController()
@@ -31,15 +36,23 @@ class MenuController: BaseController {
 //        slideMenuController()?.changeMainViewController(navigation, close: true)
     }
     @IBAction func tappedSettingButton(_ sender: Any) {
-        //let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        //let homeController = storyboard.instantiateViewController(withIdentifier: "BRTxHistory")
+        if optionSelected == 3 {
+            slideMenuController()?.closeLeft()
+            return
+        }
         let controller = TxHistoryController.shared
         let nav = UINavigationController(rootViewController: controller)
         slideMenuController()?.changeMainViewController(nav, close: true)
+        optionSelected = 3
     }
     @IBAction func tappedDonationButton(_ sender: Any) {
+        if optionSelected == 4 {
+            slideMenuController()?.closeLeft()
+            return
+        }
         let controller = DonationController(nibName:"Donation", bundle:nil)
         let navigation = UINavigationController(rootViewController: controller)
         slideMenuController()?.changeMainViewController(navigation, close: true)
+        optionSelected = 4
     }
 }
